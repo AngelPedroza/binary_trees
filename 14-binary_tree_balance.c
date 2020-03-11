3ddac98aa9095bfb52ac07d390749f3f466d6bf0
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-03-11 15:10:48 -0500

    [Add]

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,30 @@
 #include "binary_trees.h"
+#include <stdio.h>
+
+/**
+ * binary_tree_height -
+ * @tree -
+ * Return:
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t levelr = 0, levell = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+	{
+		levell = binary_tree_height(tree->left);
+		levell += 1;
+	}
+	if (tree->right)
+	{
+		levelr = binary_tree_height(tree->right);
+		levelr += 1;
+	}
+	return (levelr > levell ? levelr : levell);
+}
 
 /**
  * binary_tree_balance -
@@ -7,5 +33,17 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	
+	size_t TL, TR;
+
+	if (!tree)
+		return (0);
+
+	TL = binary_tree_height(tree->left);
+	if (tree->left && TL == 0)
+		TL = 1;
+	TR = binary_tree_height(tree->right);
+	if (tree->right && TR == 0)
+		TR = 1;
+
+	return (TL - TR);
 }
