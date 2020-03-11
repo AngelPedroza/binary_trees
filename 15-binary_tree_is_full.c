beca016af9c4d2d8df09873b20649db3f7b18f9d
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-03-11 16:29:21 -0500

    [Add] Verificate the return of both arms

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full -
+ * @tree -
+ * Return:
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+        if (!tree->left && !tree->right)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->right) && binary_tree_is_full(tree->left));
+	return (0);
+}
