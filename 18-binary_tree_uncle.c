3b60321815a77e286d8e16c8558ffdbd50cfe185
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-03-11 18:31:19 -0500

    [Add] Like the 17 task

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height -
+ * @tree -
+ * Return:
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+
+	if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+
+	return (NULL);
+}
