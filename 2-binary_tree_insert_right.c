faf0577d77986e8f27f8b41b61276d93eddafa4f
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-03-11 14:25:46 -0500

    [Fix] Nulls

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,9 +2,8 @@
 
 /**
  * binary_tree_insert_right - Function that inserts a node as right-child
- * @parent - Parent node of the node to create
- * @value - Value of the new node
- *
+ * @parent: Parent node of the node to create
+ * @value: Value of the new node
  * Return: Address of the new node
  */
 
@@ -12,14 +11,17 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = NULL;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (!new || !parent)
-	{
+	if (!parent)
 		return (NULL);
-	}
+
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+
 	new->n = value;
 	new->parent = parent;
-
+	new->right = NULL;
+	new->left = NULL;
 	if (parent->right)
 	{
 		new->right = parent->right;
