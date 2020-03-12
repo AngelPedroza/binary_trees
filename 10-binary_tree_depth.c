738c32aa212c1f5142984bc25ef11e0f6ade5770
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2020-03-12 00:06:55 +0000

    Depth of a node

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height -
- * @tree -
- * Return:
+ * binary_tree_depth - Function that measures the depth of a binary tree
+ * @tree: Root node of the tree to meadure the depth
+ *
+ * Return: depth of the node
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
