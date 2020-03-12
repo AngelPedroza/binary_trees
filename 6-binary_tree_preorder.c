6b85a3a63a450b93be860d42dd3e344b2596587c
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2020-03-12 00:05:41 +0000

    Pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder -
- * @parent -
- * @value -
- * Return:
+ * binary_tree_preorder - Function that through a BT using pre-order traversal
+ * @tree: Root node of the tree to traverse
+ * @func: Function that print number
+ *
+ * Return: Nothing
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
