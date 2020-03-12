29910c1f16280fd732e87792f6d815d4ad586778
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2020-03-12 00:06:43 +0000

    Height of a node

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height -
- * @tree -
- * Return:
+ * binary_tree_height - Function that measures the height of a binary tree
+ * @tree: Root node of the tree to meadure the height
+ *
+ * Return: Height of the node
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
