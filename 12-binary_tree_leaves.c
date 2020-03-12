c1a056d24540acdf6bb6d284ca251ed7e00c91ee
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2020-03-12 00:07:28 +0000

    Number of leaves of a binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves -
- * @tree -
- * Return:
+ * binary_tree_leaves - Function that counts the leaves in a binary tree
+ * @tree: Root to the node of the tree to count the number of leaves
+ *
+ * Return: Number of leaves
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
