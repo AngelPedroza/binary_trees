cb574f2a49316632c1ac4587cbda5b542096c167
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2020-03-12 00:07:10 +0000

    Size of a tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size -
- * @tree -
- * Return:
+ * binary_tree_size - Function that measures the size of a binary tree
+ * @tree: Root node of the tree to measure the size
+ *
+ * Return: Size of the node
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
