d005f8bd1f4ce523f480d7bfaae14fcc83b2d364
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2020-03-12 00:08:28 +0000

    Sibling node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height -
- * @tree -
- * Return:
+ * binary_tree_sibling - Function that finds the sibling of a node
+ * @node: Node to find the sibling
+ *
+ * Return: Pointer to the sibling node
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
