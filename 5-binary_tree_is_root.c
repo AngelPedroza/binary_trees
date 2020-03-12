b292ae74e84498d48be5c64fd87c16b9a796053a
Sebastian Chingate Cepeda <1200@holbertonschool.com>
2020-03-12 00:04:42 +0000

    Root node

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_is_root - Function that checks if a given node is a root
- * @node - Node to check
+ * @node: Node to check
  *
  * Return: 1 if node is a root, otherwise 0
  */
